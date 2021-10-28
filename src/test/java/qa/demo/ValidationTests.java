d73fb59fdd3320199ee25c22be884a7b5a90aa0f
Rastyshka <oscar112911@gmail.com>
2021-10-28 21:11:39 +0600

    added .gitignore

--- src/test/java/qa/demo/ValidationTests.java
+++ src/test/java/qa/demo/ValidationTests.java
@@ -3,11 +3,7 @@ package qa.demo;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-
 import java.io.File;
-
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
