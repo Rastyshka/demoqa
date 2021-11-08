8a01caeeffded2cc66a0736af51c42b0d524ce3e
Rastyshka <oscar112911@gmail.com>
2021-11-08 20:27:58 +0600

    add check results

--- src/test/java/qa/demo/pages/RegistrationPage.java
+++ src/test/java/qa/demo/pages/RegistrationPage.java
@@ -1,12 +1,18 @@
 package qa.demo.pages;
 
+import com.codeborne.selenide.SelenideElement;
 import qa.demo.pages.components.CalendarComponent;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class RegistrationPage {
+
+    private SelenideElement resultsTable = $(".table-responsive");
 
     public RegistrationPage openPage() {
         //open browser
@@ -14,49 +20,80 @@ public class RegistrationPage {
         return this;
     }
 
-     public RegistrationPage typeFirstName(String firstName){
-         $("#firstName").setValue(firstName);
-         return this;
-     }
+    public RegistrationPage typeFirstName(String firstName) {
+        $("#firstName").setValue(firstName);
+        return this;
+    }
 
-    public RegistrationPage typeLastName(String lastName){
+    public RegistrationPage typeLastName(String lastName) {
         $("#lastName").setValue(lastName);
         return this;
     }
-    public RegistrationPage typeEmail(String userEmail){
+
+    public RegistrationPage typeEmail(String userEmail) {
         $("#userEmail").setValue(userEmail);
         return this;
     }
 
-    public RegistrationPage chooseGender(String gender) {
-        $(".custom-control-label").find(byText(gender)).click();
+    public RegistrationPage chooseGender() {
+        $("[for='gender-radio-1']").click();
         return this;
     }
 
-    public RegistrationPage typeNumber(String userNumber){
-        $("#userEmail").setValue(userNumber);
+    public RegistrationPage typeNumber(String userNumber) {
+        $("#userNumber").setValue(userNumber);
         return this;
     }
 
     public RegistrationPage setDate(String day, String month, String year) {
-
         new CalendarComponent().setDate(day, month, year);
         return this;
     }
 
     public RegistrationPage typeSubject(String key, String subject) {
-
         $("#subjectsInput").click();
         $("#subjectsInput").sendKeys(key);
         $(byText(subject)).click();
         return this;
     }
 
-    public RegistrationPage chooseHobbies
+    public RegistrationPage chooseHobbies(String hobby) {
+        $(byText(hobby)).click();
+        return this;
+    }
 
+    public RegistrationPage uploadImage(String path) {
+        File img = new File(path);
+        $("#uploadPicture").uploadFile(img);
+        return this;
+    }
 
+    public RegistrationPage typeAddress(String address) {
+        $("#currentAddress").setValue(address);
+        return this;
+    }
 
+    public RegistrationPage chooseState(String state) {
+        $("#state").scrollTo().click();
+        $("#state").find(byText(state)).click();
+        return this;
+    }
 
+    public RegistrationPage chooseCity(String city) {
+        $("#city").scrollTo().click();
+        $("#city").find(byText(city)).click();
+        return this;
+    }
+
+    public void submit() {
+        $("#submit").click();
+    }
+
+    public RegistrationPage checkResultsValue(String key, String value) {
+        resultsTable.$(byText(key))
+                .sibling(0).shouldHave(text(value));
+        return this;
+    }
 
 
 }
--- src/test/java/qa/demo/tests/ValidationTests.java
+++ src/test/java/qa/demo/tests/ValidationTests.java
@@ -3,18 +3,12 @@ package qa.demo.tests;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.util.Locale;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 
 
 public class ValidationTests extends TestBase {
 
-    Faker faker = new Faker(new Locale("ru"));
+    Faker faker = new Faker(new Locale("en"));
 
     String firstName = faker.name().firstName(),
             lastName = faker.name().lastName(),
@@ -24,66 +18,37 @@ public class ValidationTests extends TestBase {
             state = "Haryana",
             city = "Karnal";
 
+
     @Test
     void fillFormTest() {
 
-        //open page
+        //fill form
         registrationPage.openPage()
                 .typeFirstName(firstName)
                 .typeLastName(lastName)
                 .typeEmail(email)
-                .chooseGender("Male")
+                .chooseGender()
                 .typeNumber(phoneNumber)
                 .setDate("20", "March", "1996")
-                .typeSubject("M", "Math")
-                        .
-                
+                .typeSubject("M", "Maths")
+                .chooseHobbies("Sports")
+                .uploadImage("src/test/resources/img/img.png")
+                .typeAddress(address)
+                .chooseState(state)
+                .chooseCity(city)
+                .submit();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //choose hobbies
-        $("[for='hobbies-checkbox-1']").click();
-
-        //upload image
-        File img = new File("src/test/resources/img/img.png");
-        $("#uploadPicture").uploadFile(img);
-
-        //fill adress
-        $("#currentAddress").setValue("some adress");
-
-        //Choose state
-        $("#state").scrollTo().click();
-        $("#state").find(byText("Haryana")).click();
-
-        //Choose city
-        $("#city").click();
-        $("#city").find(byText("Karnal")).click();
-
-        //press to Submit button
-        $("#submit").click();
-
-        //check fill data
-        $(".table-responsive tr:nth-child(1) td:nth-child(2)").shouldHave(text("Some first name Some last name"));
-        $(".table-responsive").find(byText("Student Email")).parent().shouldHave(text("sample@gmail.com"));
-        $(".table-responsive").find(byText("Gender")).parent().shouldHave(text("Male"));
-        $(".table-responsive").find(byText("Mobile")).parent().shouldHave(text("7777777777"));
-        $(".table-responsive").find(byText("Date of Birth")).parent().shouldHave(text("20 March,1996"));
-        $(".table-responsive").find(byText("Subjects")).parent().shouldHave(text("Maths"));
-        $(".table-responsive").find(byText("Hobbies")).parent().shouldHave(text("Sports"));
-        $(".table-responsive").find(byText("Picture")).parent().shouldHave(text("img.png"));
-        $(".table-responsive").find(byText("Address")).parent().shouldHave(text("some adress"));
-        $(".table-responsive").find(byText("State and City")).parent().shouldHave(text("Haryana Karnal"));
+        //check results
+        registrationPage.checkResultsValue("Student Name", firstName + " " + lastName)
+                .checkResultsValue("Student Email", email)
+                .checkResultsValue("Gender", "Male")
+                .checkResultsValue("Mobile", phoneNumber)
+                .checkResultsValue("Date of Birth", "20 March,1996")
+                .checkResultsValue("Subjects", "Maths")
+                .checkResultsValue("Hobbies", "Sports")
+                .checkResultsValue("Picture", "img.png")
+                .checkResultsValue("Address", address)
+                .checkResultsValue("State and City", state + " " + city);
 
     }
 
